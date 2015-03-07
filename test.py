2b240cb28a4f1554b3f042d479ff8fcd86cf380f
danx0r <danbmil99@gmail.com>
2015-03-07 13:08:49 -0800

    capture stderr

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 import os
 os.system("python mongoo.py reset")
 os.system("python mongoo.py init")
-os.system("python mongoo.py process > log1 &")
-os.system("python mongoo.py process | tee log2")
+os.system("python mongoo.py process > log1 2>&1")
+os.system("python mongoo.py process > log2 2>&1")
 os.system("cat log1")
+os.system("cat log2")
