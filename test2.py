cc9f8fcb3e82a21cd7f702644fe44f4e8156fd53
danx0r <danbmil99@gmail.com>
2015-03-11 14:23:00 -0700

    4X goods

--- test2.py
+++ test2.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python
-import os
-os.system("rm log1 log2")
+import os, time
+os.system("rm log1 log2 log3 log4")
 os.system("echo y | python mongoo.py config=config_test2 reset")
 os.system("python mongoo.py config=config_test2 init")
 os.system("python mongoo.py config=config_test2 process > log1 2>&1 &")
-os.system("python mongoo.py config=config_test2 process > log2 2>&1")
+os.system("python mongoo.py config=config_test2 process > log2 2>&1 &")
+os.system("python mongoo.py config=config_test2 process > log3 2>&1 &")
+os.system("python mongoo.py config=config_test2 process > log4 2>&1")
+time.sleep(3)           #make sure it's all done
 print "----LOG1----"
 os.system("cat log1")
 print
 print "----LOG2----"
 os.system("cat log2")
 print
+print "----LOG3----"
+os.system("cat log3")
+print
+print "----LOG4----"
+os.system("cat log4")
+print
 print
 print
 os.system("python mongoo.py config=config_test2 status")
-os.system("grep seconds log1 log2")
+os.system("grep seconds log1 log2 log3 log4")
