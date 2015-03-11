37d7ca6baa606f7a8688766f8e52017bfc2d278a
danx0r <danbmil99@gmail.com>
2015-03-11 14:53:44 -0700

    6X remote

---
+++ config_test2_remote.py
@@ -0,0 +1,15 @@
+# src_db = "mongodb://127.0.0.1/local_db"
+# source = 'goosrc'
+# dest_db = "mongodb://127.0.0.1/local_db"
+# dest = 'goodest'
+# test = True
+
+# src_db = "mongodb://127.0.0.1/local_db"
+src_db = "mongodb://tester:Org$tars3141592653589793@127.0.0.1:8501/dev_testdb"
+source = 'raw_scrapes'
+dest_db = "mongodb://127.0.0.1/local_db"
+dest = 'parsed'
+test = False
+chunk = 100
+waitsleep = 0.1
+
--- test2.py
+++ test2.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 import os, time
-os.system("rm log1 log2 log3 log4")
+os.system("rm log1 log2 log3 log4 log5 log6")
 os.system("echo y | python mongoo.py config=config_test2 reset")
 os.system("python mongoo.py config=config_test2 init")
 os.system("python mongoo.py config=config_test2 process > log1 2>&1 &")
 os.system("python mongoo.py config=config_test2 process > log2 2>&1 &")
 os.system("python mongoo.py config=config_test2 process > log3 2>&1 &")
-os.system("python mongoo.py config=config_test2 process > log4 2>&1")
+os.system("python mongoo.py config=config_test2 process > log4 2>&1 &")
+os.system("python mongoo.py config=config_test2 process > log5 2>&1 &")
+os.system("python mongoo.py config=config_test2 process > log6 2>&1")
 time.sleep(3)           #make sure it's all done
 print "----LOG1----"
 os.system("cat log1")
@@ -23,4 +25,4 @@ print
 print
 print
 os.system("python mongoo.py config=config_test2 status")
-os.system("grep seconds log1 log2 log3 log4")
+os.system("grep seconds log1 log2 log3 log4 log5 log6")
