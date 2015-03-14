53d076e619a5c004ba43db7dd2ae5ceb48d412c8
danx0r <danbmil99@gmail.com>
2015-03-14 13:45:35 -0700

    fix make_goosrc

--- make_goosrc.py
+++ make_goosrc.py
@@ -1,9 +1,12 @@
 #
 # test mongoo
 #
-import sys, os
+import sys, os, importlib
 import mongoengine as meng
-import config
+if len(sys.argv) > 1 and 'config=' in sys.argv[1]:
+    config = importlib.import_module(sys.argv[1][7:])
+else:
+    import config
 
 from goosrc_goodest import goosrc
 from mongoo import connect2db
@@ -12,7 +15,7 @@ connect2db(goosrc, config.src_db)
 if raw_input("drop goosrc?")[:1] == 'y':
     goosrc.drop_collection()
 
-for i in range(0, int(sys.argv[1]), 3):
+for i in range(0, int(sys.argv[-1]), 3):
     g = goosrc(num = i)
     g.save()
 
