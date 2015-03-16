46692bdf5b225df1f7d6a24753a1266e26ec3a61
danx0r <dan.miller@hiqlabs.com>
2015-03-16 13:09:26 -0700

    setup for use as dependency in requirements.txt

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='mongoo',
+      version='0.001',
+      description='Mongo Operations',
+      url='https://github.com/gitorgstars/mongoo/',
+     )
