1a85f5c3ff9442ddaee18c9d2483debe94f220d9
danx0r <dan.miller@hiqlabs.com>
2016-05-20 15:25:51 -0700

    ultra slimp

--- goosrc_goodest.py
+++ goosrc_goodest.py
@@ -1,38 +1,13 @@
-#
-# Template mongoo processing script
-#
-import sys, os, time, random, traceback
 import pymongo
-#
-# source field to use for partitioning (must be unique and orderable)
-#
-KEY = '_id'
-#
-# optional query:
-#
-# QUERY = {'num__ne': 9}
-#
-# called on initialization of processing (incremental)
-#
-# def init():
-#     pass
-#
-# called on full reset
-#
-# def reset():
-#     pass
-#
-# called for each chunk (concurrent)
-# source is mongoengine cursor; dest is mongoengine object class
-# both are associated with db connections
-#
-def process(myid, keylist, source_uri, source_col, dest_uri, dest_col, *args, **kw):
-    print myid, "process %d documents from %s to %s" % (len(keylist), source_col, dest_col)
-    source = pymongo.MongoClient(source_uri).get_default_database()[source_col]
-    dest = pymongo.MongoClient(dest_uri).get_default_database()[dest_col]
-    for key in keylist:
-        src = source.find_one({KEY: key})
-        dest.save({'_id': src[KEY]*10})
+
+def process(source, dest):  #type(source)=cursor, type(dest)=collection
+    print "process %d documents from %s to %s" % (source.count(), source.collection.name, dest.name)
+    for doc in source:
+        dest.save({'_id': doc['_id']*10})
+        print "  processed %s" % doc['_id']
 
 if __name__ == "__main__":
-    process(1, [0, 3, 6], "mongodb://127.0.0.1/local_db", "goosrc", "mongodb://127.0.0.1/local_db", "goodest")
+    db = pymongo.MongoClient().test
+    source = db.goosrc.find()
+    dest = db.goodest
+    process(source, dest)
