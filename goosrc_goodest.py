3c6f5202ccb0df5bd69a96e5e85252dcd32b09fa
danx0r <dan.miller@hiqlabs.com>
2016-05-20 14:05:59 -0700

    rework goosrc_goodest.py to rm meng

--- goosrc_goodest.py
+++ goosrc_goodest.py
@@ -2,20 +2,7 @@
 # Template mongoo processing script
 #
 import sys, os, time, random, traceback
-import mongoengine as meng
-# PYBASE = os.path.abspath(os.path.join(os.path.dirname(__file__), "../science") ) 
-# sys.path.append(PYBASE)
-
-#
-# in real-world app, we would import our source and destination collections
-#
-class goosrc(meng.Document):
-#     num = meng.IntField(primary_key = True)
-    num = meng.IntField()
-
-class goodest(meng.Document):
-    numnum = meng.StringField()
-
+import pymongo
 #
 # source field to use for partitioning (must be unique and orderable)
 #
@@ -43,35 +30,40 @@ QUERY = {'num__ne': 9}
 # source is mongoengine cursor; dest is mongoengine object class
 # both are associated with db connections
 #
-def process(source, dest, myid):
-    time.sleep(2)                   #just for testing
+def process(myid, keylist, source_uri, source_col, dest_uri, dest_col, *args, **kw):
     log = []
     good = bad = 0
-    print myid, "  process %d from" % source.count(), source._collection, "to", dest.objects._collection
-    for x in source:
-        dbg1 = source._collection.database.test_mongoo.find_and_modify({'_id':'dbg1'}, {'$inc':{'n':1}})['n']
+    source = pymongo.MongoClient(source_uri).get_default_database()
+    dest = pymongo.MongoClient(dest_uri).get_default_database()
+    print myid, "  process %d documents from %s,%s to %s,%s" % (len(keylist), source, source_col, dest, dest_col)
+    for key in keylist:
+        dbg1 = source['test_mongoo'].find_and_modify({'_id':'dbg1'}, {'$inc':{'n':1}})['n']
         if dbg1 == 11:               #emulate untrapped segfault/lost or dead process. It happens
             print myid, "INEXPLICABLE FAIL"
             exit()
+        src = source[source_col].find_one({KEY: key})
         try:
-            if x.num != 24:
-                print myid, "    goosrc_goodest.process:", x.num
+            num = src['num']
+            if num != 24:
+                print myid, "    goosrc_goodest.process:", num
                 sys.stdout.flush()
-                d = dest()
-                d.numnum = str(x.num*10)
-                d.save()
+#                 d = dest()
+#                 d.numnum = str(x.num*10)
+                dest[dest_col].save({'numnum': num})
             else:
-                print myid, "    goosrc_goodest: skipping", x.num
+                print myid, "    goosrc_goodest: skipping", num
                 sys.stdout.flush()
                 raise Exception("spurious exception")
             good += 1
         except:
             print myid, "------------ERROR--------------"
             bad += 1
-            err = "exception processing %s\n" % x.num
-            err += traceback.format_exc()
+            err = traceback.format_exc()
             log.append(err)
             print myid, err
             print myid, "-------------------------------"
             sys.stdout.flush()
     return good, bad, log
+
+if __name__ == "__main__":
+    process(1, [0, 3], "mongodb://127.0.0.1/local_db", "goosrc", "mongodb://127.0.0.1/local_db", "goodest")
