a1a5292ad2fafb3ff7fb37bab45705c8363848df
danx0r <dan.miller@hiqlabs.com>
2016-05-20 16:26:09 -0700

    ultra w meng

--- goosrc_goodest.py
+++ goosrc_goodest.py
@@ -6,6 +6,22 @@ def process(source, dest):  #type(source)=cursor, type(dest)=collection
         dest.save({'_id': doc['_id']*10})
         print "  processed %s" % doc['_id']
 
+# meng?
+import mongoengine as meng
+
+class goosrc(meng.Document):
+    _id = meng.IntField(primary_key = True)
+
+class goodest(meng.Document):
+    _id = meng.IntField(primary_key = True)
+
+def process(source, dest):
+    meng.connect("test")
+    for doc in source:
+        gs = goosrc.objects(_id = doc['_id'])[0]
+        gd = goodest(id = gs.id * 10)
+        gd.save()
+
 if __name__ == "__main__":
     db = pymongo.MongoClient().test
     source = db.goosrc.find()
