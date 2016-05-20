8c2bc9086be3b3061c90835262ec19ba15bd7ce3
danx0r <dan.miller@hiqlabs.com>
2016-05-20 14:34:18 -0700

    un-meng make_goosrc

--- make_goosrc.py
+++ make_goosrc.py
@@ -2,34 +2,22 @@
 # test mongoo
 #
 import sys, os, importlib, argparse
-import mongoengine as meng
 import pymongo
-# if len(sys.argv) > 1 and 'config=' in sys.argv[1]:
-#     config = importlib.import_module(sys.argv[1][7:])
-# else:
-#     import config
 
 par = argparse.ArgumentParser(description = "create test data for mongoo")
 par.add_argument("src_db")
 par.add_argument("num", type = int)
 config = par.parse_args()
 
-from goosrc_goodest import goosrc
-from mongoo import connect2db
+db = pymongo.MongoClient(config.src_db).get_default_database()
+print "db:", db
 
-db = connect2db(goosrc, config.src_db)
 if raw_input("drop goosrc?")[:1] == 'y':
-    goosrc.drop_collection()
+    db.goosrc.drop()
 
 for i in range(0, int(config.num), 3):
-    g = goosrc(num = i)
-    g.save()
-    if i == 6:
-        g = goosrc(num = i)
-        g.save()
-        g = goosrc(num = i)
-        g.save()
-print "created %d objects" % goosrc.objects.count()
+    db.goosrc.save({'num': i})
+print "created %d objects" % db.goosrc.count()
 
 #for debug tests
 db.test_mongoo.save({'_id':'dbg1','n':1})
