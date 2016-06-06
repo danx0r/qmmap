293a188334686fc1ce252fc2daecb20db887f35a
danx0r <dan.miller@hiqlabs.com>
2016-06-06 16:56:32 -0700

    test all paths

--- test_pymongo.py
+++ test_pymongo.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     import os, pymongo, mongoo, time
     print "DEBUG test_pymongo pid:", os.getpid()
     os.system("python make_goosrc.py mongodb://127.0.0.1/test 10")
-    mongoo.mmap(process, "goosrc", "goodest", multi=1)
+    mongoo.mmap(process, "goosrc", "goodest", multi=None)
     time.sleep(4)
     db = pymongo.MongoClient("mongodb://127.0.0.1/test").get_default_database()
     print "output:"
