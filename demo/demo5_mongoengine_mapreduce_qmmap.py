fc128731c041f588ee61aef6dcc21bf7c5ee6d6a
danx0r <dan.miller@eye0.com>
2018-07-20 16:01:53 -0700

    one idea

---
+++ demo/demo5_mongoengine_mapreduce_qmmap.py
@@ -0,0 +1,43 @@
+# assumes mongodb running locally, database named 'test', function input is a
+# pymongo object
+
+import sys, os
+PYBASE = os.path.abspath(os.path.join(os.path.dirname(__file__), "..") ) 
+sys.path.insert(0, PYBASE)
+
+import pymongo
+from mongoengine import Document, IntField, connect
+
+from qmmap import toMongoEngine, connectMongoEngine, mmap
+
+
+# connect("test") ###not needed if multi>1
+
+class qmmap_in(Document):
+    num = IntField(primary_key = True)
+    extra = IntField()
+
+class qmmap_out(Document):
+    val = IntField(primary_key = True)
+    comp = IntField()
+
+def init(source, dest):
+    print ("A",dest)
+    global g_dest
+    g_dest=dest                    #pass to func in global space for each chunk/process
+
+def func(source):
+    print ("FUNC:", g_dest, g_dest.count())
+    gs = toMongoEngine(source, qmmap_in)
+    times10 = gs.num * 10
+    gd = qmmap_out(val=times10, comp=times10 + gs.extra)
+    return gd.to_mongo()
+
+db = pymongo.MongoClient().test
+for i in range(10):
+    db.qmmap_in.save({'_id': i, 'extra': i + 1})
+
+ret = mmap(func, "qmmap_in", "qmmap_out", multi=1, sleep=2, reset=True,  init=init)
+
+for o in qmmap_out.objects:
+    print((o.val, o.comp))
